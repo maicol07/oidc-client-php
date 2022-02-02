0ee533edd6d5f5afbb17993cbcf03f860c6ce84d
Maicol Battistini <maicolbattistini@live.it>
2022-02-02 15:00:54 +0100

    feat: ✨ Added support for file keys and RSA, ECDSA signing methods

--- src/Traits/JWT.php
+++ src/Traits/JWT.php
@@ -1,4 +1,5 @@
-<?php
+<?php /** @noinspection PhpUnusedPrivateMethodInspection */
+
 /*
  * Copyright 2022 Maicol07 (https://maicol07.it)
  *
@@ -19,10 +20,13 @@ namespace Maicol07\OpenIDConnect\Traits;
 
 use DateInterval;
 use DateTimeZone;
+use Illuminate\Support\Str;
 use Lcobucci\Clock\SystemClock;
 use Lcobucci\JWT\Configuration;
+use Lcobucci\JWT\Signer\Ecdsa;
 use Lcobucci\JWT\Signer\Hmac;
 use Lcobucci\JWT\Signer\Key\InMemory;
+use Lcobucci\JWT\Signer\Rsa;
 use Lcobucci\JWT\Validation\Constraint\IssuedBy;
 use Lcobucci\JWT\Validation\Constraint\PermittedFor;
 use Lcobucci\JWT\Validation\Constraint\SignedWith;
@@ -33,6 +37,8 @@ use Maicol07\OpenIDConnect\ClientException;
 trait JWT
 {
     private string $jwt_signing_method;
+    /** @var string|null Only needed if signing method is set to RSXXX or ECXXX. */
+    private ?string $jwt_signing_key;
     private ?string $jwt_key;
     private bool $jwt_plain_key;
     private int $leeway;
@@ -52,15 +58,24 @@ trait JWT
     private function jwt(): Configuration
     {
         $signer = match ($this->jwt_signing_method) {
-            'sha256' => new Hmac\Sha256(),
-            'sha384' => new Hmac\Sha384(),
-            'sha512' => new Hmac\Sha512()
+            'HS256', 'sha256' => new Hmac\Sha256(),
+            'HS384', 'sha384' => new Hmac\Sha384(),
+            'HS512', 'sha512' => new Hmac\Sha512(),
+            'RS256' => new Rsa\Sha256(),
+            'RS384' => new Rsa\Sha384(),
+            'RS512' => new Rsa\Sha512(),
+            'EC256' => Ecdsa\Sha256::create(),
+            'EC384' => Ecdsa\Sha384::create(),
+            'EC512' => Ecdsa\Sha512::create(),
         };
 
-        $config = Configuration::forSymmetricSigner(
-            $signer,
-            $this->jwt_plain_key ? InMemory::plainText($this->jwt_key) : InMemory::base64Encoded($this->jwt_key)
-        );
+        $key = $this->getJWTKey($this->jwt_key);
+
+        if (Str::startsWith($this->jwt_signing_method, ['RS', 'EC'])) {
+            $config = Configuration::forAsymmetricSigner($signer, $this->getJWTKey($this->jwt_signing_key), $key);
+        } else {
+            $config = Configuration::forSymmetricSigner($signer, $key);
+        }
 
         $config->setValidationConstraints(
             new PermittedFor($this->client_id),
@@ -75,5 +90,18 @@ trait JWT
         );
 
         return $config;
+    }
+
+    private function getJWTKey(string $key): InMemory
+    {
+        if (file_exists($key)) {
+            return InMemory::file($key);
+        }
+
+        if ($this->jwt_plain_key) {
+            return InMemory::plainText($key);
+        }
+
+        return InMemory::base64Encoded($key);
     }
 }
