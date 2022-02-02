9ff9b8f91fee6f035b0a14272fa21c58d46b8bd5
Maicol Battistini <maicolbattistini@live.it>
2022-02-02 14:38:38 +0100

    refactor: Split main Client class in Traits

--- src/Client.php
+++ src/Client.php
@@ -1,32 +1,37 @@
 <?php
+/*
+ * Copyright 2022 Maicol07 (https://maicol07.it)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 /** @noinspection PhpUnused */
 /** @noinspection PhpPropertyOnlyWrittenInspection */
 
 namespace Maicol07\OpenIDConnect;
 
-use cse\helpers\Session;
-use DateInterval;
-use DateTimeZone;
 use Exception;
 use Illuminate\Http\Client\Factory;
 use Illuminate\Http\Client\PendingRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Arr;
-use Illuminate\Support\Collection;
-use Illuminate\Support\Str;
 use JetBrains\PhpStorm\NoReturn;
-use Lcobucci\Clock\SystemClock;
-use Lcobucci\JWT\Configuration;
-use Lcobucci\JWT\Signer\Hmac;
-use Lcobucci\JWT\Signer\Key\InMemory;
-use Lcobucci\JWT\Validation\Constraint\IssuedBy;
-use Lcobucci\JWT\Validation\Constraint\PermittedFor;
-use Lcobucci\JWT\Validation\Constraint\SignedWith;
-use Lcobucci\JWT\Validation\Constraint\StrictValidAt;
-use Lcobucci\JWT\Validation\RequiredConstraintsViolated;
-use function bin2hex;
-use function random_bytes;
+use Maicol07\OpenIDConnect\Traits\Authorization;
+use Maicol07\OpenIDConnect\Traits\AutoDiscovery;
+use Maicol07\OpenIDConnect\Traits\DynamicRegistration;
+use Maicol07\OpenIDConnect\Traits\ImplictFlow;
+use Maicol07\OpenIDConnect\Traits\JWT;
+use Maicol07\OpenIDConnect\Traits\Token;
 
 /**
  *
@@ -35,18 +40,20 @@ use function random_bytes;
  */
 class Client
 {
+    use Authorization;
+    use Token;
+    use AutoDiscovery;
+    use DynamicRegistration;
+
+    use ImplictFlow;
+    use JWT;
+
     private string $client_id;
-    private string $client_name;
     private string $client_secret;
     private ?string $issuer;
     private string $access_token;
-    private ?string $refresh_token;
     private string $id_token;
     private array $scopes;
-    private array $response_types;
-    private int $leeway;
-    /** Allow OAuth 2 implicit flow; see http://openid.net/specs/openid-connect-core-1_0.html#ImplicitFlowAuth */
-    private bool $allow_implicit_flow = false;
     private string $redirect_uri;
 
     /**
@@ -54,26 +61,14 @@ class Client
      * @see https://tools.ietf.org/html/rfc7636
      */
     private string $code_challenge_method;
-    private array $pkce_algorithms = ['S256' => 'sha256', 'plain' => false];
     private bool $enable_pkce;
     private bool $enable_nonce;
 
     private PendingRequest $http_client;
 
     // Endpoints
-    private string $authorization_endpoint;
-    private bool $authorization_response_iss_parameter_supported;
-    private string $token_endpoint;
-    private array $token_endpoint_auth_methods_supported;
     private string $userinfo_endpoint;
     private ?string $end_session_endpoint;
-    private ?string $registration_endpoint;
-    private ?string $introspect_endpoint;
-    private ?string $revocation_endpoint;
-
-    private string $jwt_signing_method;
-    private ?string $jwt_key;
-    private bool $jwt_plain_key;
 
     /**
      * @param array {
@@ -120,27 +115,12 @@ class Client
             'verify' => Arr::get($user_config, 'verify', true) ?: Arr::get($user_config, 'cert_path', false)
         ]);
 
-        // Auto discovery
         $provider_url = rtrim(Arr::get($user_config, 'provider_url'), '/');
+        $query_params = Arr::get($user_config, 'well_known_request_params');
 
-        if ($provider_url) {
-            $discovery_path = '.well-known/openid-configuration';
-            $discovery_endpoint = "$provider_url/$discovery_path";
+        $config = $this->autoDiscovery($provider_url, $query_params)?->merge($user_config) ?? collect($user_config);
 
-            if (Str::endsWith($provider_url, $discovery_path)) {
-                $discovery_endpoint = $provider_url;
-                $provider_url = Str::replace($provider_url, $discovery_path, '');
-            }
-
-            $response = $this->http_client
-                ->get($discovery_endpoint, Arr::get($user_config, 'well_known_request_params'));
-
-            if ($response->ok()) {
-                $config = $response->collect()->merge($user_config);
-            }
-        }
-
-        $config ??= collect($user_config);
+        $provider_url = $this->trimDiscoveryPath($provider_url);
 
         $props = [
             'client_id' => null,
@@ -191,6 +171,24 @@ class Client
     {
         $request = Request::capture();
 
+        $this->validateCallback($request);
+
+        // If we have an authorization code then proceed to request a token
+        $code = $request->get('code');
+        if ($code) {
+            return $this->token($request, $code);
+        }
+
+        $id_token = $request->get('id_token');
+        if ($this->allow_implicit_flow && $id_token) {
+            $this->implictFlow($request, $id_token);
+        }
+
+        $this->requestAuthorization();
+    }
+
+    private function validateCallback(Request $request): void
+    {
         // protect against mix-up attacks
         // experimental feature, see https://tools.ietf.org/html/draft-ietf-oauth-iss-auth-resp-00
         if ($this->authorization_response_iss_parameter_supported && $request->hasAny(['error', 'code', 'id_token'])
@@ -204,83 +202,6 @@ class Client
             $description = ' Description: ' . $request->get('error_description', 'No description provided');
             throw new ClientException('Error: ' . $request->get('error') . $description);
         }
-
-        $jwt_config = $this->jwt();
-
-        // If we have an authorization code then proceed to request a token
-        $code = $request->get('code');
-        if ($code) {
-            $token_response = $this->requestTokens($code);
-
-            // Throw an error if the server returns one
-            $error = $token_response->get('error');
-            if ($error) {
-                $description = $token_response->get('error_description');
-                throw new ClientException($description ?: ('Got response: ' . $error));
-            }
-
-            // Do an OpenID Connect session check
-            if ($request->get('state') !== Session::get('oidc_state')) {
-                throw new ClientException('Unable to determine state');
-            }
-            Session::remove('oidc_state');
-
-            if (!$token_response->has('id_token')) {
-                throw new ClientException('User did not authorize openid scope.');
-            }
-
-            if ($this->enable_nonce && Session::get('oidc_nonce') !== $request->get('nonce')) {
-                throw new ClientException("Generated nonce is not equal to the one returned by the server.");
-            }
-            Session::remove('oidc_nonce');
-
-            try {
-                $jwt = $jwt_config->parser()->parse($token_response->get('id_token'));
-                $jwt_config->validator()->assert($jwt, ...$jwt_config->validationConstraints());
-            } catch (RequiredConstraintsViolated $e) {
-                throw new ClientException(
-                    'JWT validation error - Claims not valid: ' . implode(', ', $e->violations())
-                );
-            }
-
-            $this->id_token = $token_response->get('id_token');
-            $this->access_token = $token_response->get('access_token');
-            $this->refresh_token = $token_response->get('refresh_token');
-
-            return true;
-        }
-
-        $id_token = $request->get('id_token');
-        if ($this->allow_implicit_flow && $id_token) {
-            $this->access_token = $request->get('access_token');
-
-            // Do an OpenID Connect session check
-            if ($request->get('state') !== Session::get('oidc_state')) {
-                throw new ClientException('Unable to determine state');
-            }
-            Session::remove('oidc_state');
-
-            try {
-                $jwt = $jwt_config->parser()->parse($id_token);
-                $jwt_config->validator()->assert($jwt, ...$jwt_config->validationConstraints());
-            } catch (RequiredConstraintsViolated $e) {
-                throw new ClientException(
-                    'JWT validation error - Claims not valid: ' . implode(', ', $e->violations())
-                );
-            }
-
-            // Save the id token
-            $this->id_token = $id_token;
-
-            if ($this->enable_nonce && $request->get('nonce') === Session::get('oidc_nonce')) {
-                return true;
-            }
-            Session::remove('oidc_nonce');
-
-            throw new ClientException('Unable to verify JWT claims');
-        }
-
-        $this->requestAuthorization();
     }
 
     /**
@@ -313,179 +234,6 @@ class Client
     }
 
     /**
-     * Get the authorization URL
-     *
-     * @throws Exception
-     */
-    public function getAuthorizationUrl(?array $query_params = null, ?string $state = null): string
-    {
-        $auth_endpoint = $this->authorization_endpoint;
-
-        // State essentially acts as a session key for OIDC
-        $state = $state ?? Str::random();
-        Session::set('oidc_state', $state);
-
-        $params = collect([
-            'response_type' => 'code',
-            'redirect_uri' => $this->redirect_uri,
-            'client_id' => $this->client_id,
-            'state' => $state,
-            'scope' => implode(' ', array_merge($this->scopes, ['openid']))
-        ])->merge($query_params);
-
-        if ($this->enable_nonce) {
-            $nonce = Str::random();
-            Session::set('oidc_nonce', $nonce);
-            $params->put('nonce', $nonce);
-        }
-
-        // If the client has been registered with additional response types
-        if (count($this->response_types) > 0) {
-            $params->put('response_type', implode(' ', $this->response_types));
-        }
-
-        // If the OP supports Proof Key for Code Exchange (PKCE) and it is enabled
-        // PKCE will only used in pure authorization code flow and hybrid flow
-        if (
-            $this->enable_pkce
-            && !empty($this->code_challenge_method)
-            && (empty($this->response_types) || count(array_diff($this->response_types, ['token', 'id_token'])) > 0)
-        ) {
-            // Generate a cryptographically secure code
-            $code_verifier = bin2hex(random_bytes(64));
-            Session::set('oidc_code_verifier', $code_verifier);
-            $code_challenge = !empty($this->pkce_algorithms[$this->code_challenge_method])
-                ? rtrim(
-                    strtr(
-                        base64_encode(
-                            hash(
-                                $this->pkce_algorithms[$this->code_challenge_method],
-                                $code_verifier,
-                                true
-                            )
-                        ),
-                        '+/',
-                        '-_'
-                    ),
-                    '='
-                )
-                : $code_verifier;
-            $params->put('code_challenge', $code_challenge)->put('code_challenge_method', $this->code_challenge_method);
-        }
-
-        $auth_endpoint .= (!str_contains($auth_endpoint, '?') ? '?' : '&') . Arr::query($params->all());
-        return $auth_endpoint;
-    }
-
-    /**
-     * Start Here
-     *
-     * @throws ClientException
-     * @throws Exception
-     */
-    #[NoReturn]
-    private function requestAuthorization(): void
-    {
-        $auth_endpoint = $this->getAuthorizationUrl();
-
-        session_write_close();
-        $this->redirect($auth_endpoint);
-    }
-
-    /**
-     * Requests ID and Access tokens
-     */
-    private function requestTokens(string $code): Collection
-    {
-        $data = [
-            'grant_type' => 'authorization_code',
-            'code' => $code,
-            'redirect_uri' => $this->redirect_uri,
-            'client_id' => $this->client_id,
-            'client_secret' => $this->client_secret
-        ];
-
-        // Consider Basic authentication if provider config is set this way
-        $client = $this->http_client;
-        if (in_array('client_secret_basic', $this->token_endpoint_auth_methods_supported, true)) {
-            $client = $client->withBasicAuth($this->client_id, $this->client_secret);
-            unset($data['client_secret'], $data['client_id']);
-        }
-
-        $code_verifier = Session::get('oidc_code_verifier');
-        if ($this->enable_pkce && !empty($this->code_challenge_method) && !empty($code_verifier)) {
-            $data['code_verifier'] = $code_verifier;
-        }
-
-        return $client->asForm()->post($this->token_endpoint, $data)->collect();
-    }
-
-    /**
-     * Requests Access token with refresh token
-     *
-     * @param bool $send_scopes (optional) Controls whether scopes are sent in the request, defaults to true
-     */
-    public function refreshToken(string $refresh_token, bool $send_scopes = true): Collection
-    {
-        $data = [
-            'grant_type' => 'refresh_token',
-            'refresh_token' => $refresh_token,
-            'client_id' => $this->client_id,
-            'client_secret' => $this->client_secret,
-        ];
-
-        if ($send_scopes) {
-            $data['scopes'] = implode(' ', $this->scopes);
-        }
-
-        $client = $this->http_client;
-
-        // Consider Basic authentication if provider config is set this way
-        if (in_array('client_secret_basic', $this->token_endpoint_auth_methods_supported, true)) {
-            $client = $client->withBasicAuth($this->client_id, $this->client_secret);
-            unset($data['client_secret'], $data['client_id']);
-        }
-
-        $response = $client->post($this->token_endpoint, $data)->collect();
-
-        $this->access_token = $response->get('access_token');
-        $this->refresh_token = $response->get('refresh_token');
-
-        return $response;
-    }
-
-    /**
-     * @throws Exception
-     */
-    private function jwt(): Configuration
-    {
-        $signer = match ($this->jwt_signing_method) {
-            'sha256' => new Hmac\Sha256(),
-            'sha384' => new Hmac\Sha384(),
-            'sha512' => new Hmac\Sha512()
-        };
-
-        $config = Configuration::forSymmetricSigner(
-            $signer,
-            $this->jwt_plain_key ? InMemory::plainText($this->jwt_key) : InMemory::base64Encoded($this->jwt_key)
-        );
-
-        $config->setValidationConstraints(
-            new PermittedFor($this->client_id),
-            new StrictValidAt(
-                new SystemClock(
-                    new DateTimeZone(date_default_timezone_get())
-                ),
-                new DateInterval("PT{$this->leeway}S")
-            ),
-            new SignedWith($config->signer(), $config->signingKey()),
-            new IssuedBy($this->issuer)
-        );
-
-        return $config;
-    }
-
-    /**
      * Returns the user info
      *
      * @throws ClientException
@@ -510,91 +258,6 @@ class Client
     {
         header('Location: ' . $url);
         exit;
-    }
-
-    /**
-     * Dynamic registration
-     *
-     * @throws ClientException
-     */
-    public function register(?array $params = null): void
-    {
-        $data = collect($params)
-            ->put('redirect_uris', [$this->redirect_uri])
-            ->put('client_name', $this->client_name);
-
-        $response = $this->http_client->post($this->registration_endpoint, $data->all())->collect();
-
-        $error = $response->get('error_description');
-        if ($error) {
-            throw new ClientException($error);
-        }
-
-        $this->client_id = $response->get('client_id');
-
-        // The OpenID Connect Dynamic registration protocol makes the client secret optional
-        // and provides a registration access token and URI endpoint if it is not present
-        $secret = $response->get('client_secret');
-        if ($secret) {
-            $this->client_secret = $secret;
-        } else {
-            throw new ClientException('Error registering: Please contact the OpenID Connect provider
-             and obtain a Client ID and Secret directly from them');
-        }
-    }
-
-    /**
-     * Introspect a given token — either access token or refresh token.
-     *
-     * @link https://tools.ietf.org/html/rfc7662
-     */
-    public function introspectToken(
-        string  $token,
-        string  $token_type_hint = '',
-        ?string $client_id = null,
-        ?string $client_secret = null
-    ): Collection
-    {
-        $data = ['token' => $token];
-
-        if ($token_type_hint) {
-            $data['token_type_hint'] = $token_type_hint;
-        }
-        $client_id ??= $this->client_id;
-        $client_secret ??= $this->client_secret;
-
-        return $this->http_client
-            ->withBasicAuth($client_id, $client_secret)
-            ->acceptJson()
-            ->post($this->introspect_endpoint, $data)
-            ->collect();
-    }
-
-    /**
-     * Revoke a given token - either access token or refresh token.
-     *
-     * @see https://tools.ietf.org/html/rfc7009
-     */
-    public function revokeToken(
-        string  $token,
-        string  $token_type_hint = '',
-        ?string $client_id = null,
-        ?string $client_secret = null
-    ): Collection
-    {
-        $data = ['token' => $token];
-
-        if ($token_type_hint) {
-            $data['token_type_hint'] = $token_type_hint;
-        }
-        $client_id ??= $this->client_id;
-        $client_secret ??= $this->client_secret;
-
-        return $this->http_client
-            ->withBasicAuth($client_id, $client_secret)
-            ->acceptJson()
-            ->post($this->revocation_endpoint, $data)
-            ->collect();
     }
 
     /** @noinspection GlobalVariableUsageInspection */
