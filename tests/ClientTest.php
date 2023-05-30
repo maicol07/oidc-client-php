f55704d1e7bd0fff081e5e3d98552e5a724c0451
Maicol Battistini <maicolbattistini@live.it>
2023-05-30 20:32:36 +0200

    test: ✅ Added implicit flow test

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -66,15 +66,19 @@ class ClientTest extends TestCase
         $driver->wait(5)->until(fn ($driver) => str_starts_with($driver->getCurrentURL(), $redirect_uri));
 
         $url = $driver->getCurrentURL();
-        $query = parse_url($url)['query'];
-        echo "Callback query: $query";
-        parse_str($query, $params);
+        $paramsString = parse_url($url)[$this->client()->allow_implicit_flow ? 'fragment' : 'query'];
+        parse_str($paramsString, $params);
 
         $driver->quit();
 
-        dump($params);
-        $this->assertArrayHasKey('code', $params);
+        dump($this->client()->allow_implicit_flow ? 'IMPLICIT FLOW' : 'STANDARD FLOW', $params);
+        if (!$this->client()->allow_implicit_flow) {
+            $this->assertArrayHasKey('code', $params);
+        }
         $this->assertArrayHasKey('state', $params);
+
+        $params['session_state'] = Session::get('oidc_state');
+        $params['session_nonce'] = Session::get('oidc_nonce');
 
         return $params;
     }
@@ -103,6 +107,45 @@ class ClientTest extends TestCase
         dump("Tokens:\nAccess token: $access_token\nRefresh token: $refresh_token\nID token: $id_token");
         return $this->client();
     }
+
+    #[Depends('testAuthorization')]
+    public function testImplicitFlow(array $params): void {
+        if (!$this->client()->allow_implicit_flow) {
+            return;
+        }
+
+        if (env('OIDC_STATE')) {
+            Session::set('oidc_state', env('OIDC_STATE'));
+        }
+
+        if (!Session::has('oidc_state')) {
+            Session::set('oidc_state', $params['session_state']);
+        }
+
+        if (!Session::has('oidc_nonce')) {
+            Session::set('oidc_nonce', $params['session_nonce']);
+        }
+
+        $request = Request::create('', parameters: [
+            'state' => env('OIDC_STATE', $params['state']),
+            'id_token' => env('OIDC_ID_TOKEN', $params['id_token']),
+            'access_token' => env('OIDC_ACCESS_TOKEN', $params['access_token']),
+            'token_type' => env('OIDC_TOKEN_TYPE', $params['token_type']),
+            'expires_in' => env('OIDC_EXPIRES_IN', $params['expires_in']),
+            'scope' => env('OIDC_SCOPE', $params['scope'])
+        ]);
+        $result = $this->invokeMethod($this->client(), 'implicitFlow', [$request, env('OIDC_ID_TOKEN', $params['id_token'])]);
+        $this->assertTrue($result);
+
+        $access_token = $this->getProperty($this->client(), 'access_token');
+        $id_token = $this->getProperty($this->client(), 'id_token');
+
+        $this->assertIsString($access_token);
+        $this->assertIsString($id_token);
+
+        dump("Tokens:\nAccess token: $access_token\nID token: $id_token");
+    }
+
 
     #[Depends('testToken')]
     public function testUserInfo(Client $client): bool {
